b01849f489672e9aafb571a4e889fca447f465ab
PringleElUno <pringlel@oregonstate.edu>
2024-10-06 13:28:31 -0700

    2c

---
+++ change.py
@@ -0,0 +1,23 @@
+print ("Please enter an amount in cents less than a dollar. ")
+cents  = int(input())
+
+Q = cents // 25
+cents -= Q * 25
+
+
+D = cents // 10
+cents -= D * 10
+
+N = cents // 5
+cents -= N * 5
+
+P = cents
+
+
+# Q= Quarters, D= Dimes, N= Nickles, P= Pennies/ Cents
+
+print ("Your change will be: ")
+print ("Q: ", Q)
+print ("D: ", D)
+print ("N: ", N)
+print ("P: ", P)
